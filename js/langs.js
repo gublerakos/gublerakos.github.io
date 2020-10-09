964db7c116f201949102f03dd00ebc8858622432
gublerakos <mariaapntz@gmail.com>
2020-10-09 13:12:43 +0300

    site xenoglossi paideia remorved from temporary server

--- js/langs.js
+++
@@ -1,6 +0,0 @@
-document.write("			if (x.style.display === \"grid\") {");
-document.write("				x.style.display = \"none\";");
-document.write("			}");
-document.write("			else {");
-document.write("				x.style.display = \"grid\";");
-document.write("			}");
--- js/menu.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-	var x = document.getElementById("mobile");
-	if (x.style.display === "grid") {
-		x.style.display = "none";
-	}
-	else {
-		x.style.display = "grid";
-	}
-}
