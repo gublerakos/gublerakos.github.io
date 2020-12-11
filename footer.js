ed1acaacd9acd63b2c71f2c47a21194ca6aa0716
gublerakos <mariaapntz@gmail.com>
2020-12-11 14:55:44 +0200

    changes

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write('    <footer>\n');
+document.write('        <div class=\"flogo\">\n');
+document.write('            <img src=\"cockatiel-silhouette-White.png\" alt=\"logo\" class=\"footerLogo\">\n');
+document.write('            <h1 class=\"footerText\">Art Eagle</h1>\n');
+document.write('        </div>\n');
+document.write('    </footer>');
